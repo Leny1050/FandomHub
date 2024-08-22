0fe10fb8fdd8544ff34af2906b2c6098efcc9ed8
Leny1050 <101010tatata1010@gmail.com>
2024-08-22 23:55:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+<script type="module">
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
     import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
     import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
     import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js";
